8c4efd30f071f36f5ade06545d8e15dca653e2ef
Rodriasdev <veronrodrigo98@gmail.com>
2024-05-23 19:17:54 -0300

    added the file 'models'

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import {createServer} from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer();
+const io = new Server(server)
 
 const PORT = process.env.PORT || 4000;
 
