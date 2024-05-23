1731c9ae75692f908a65231222fda981cab4975b
Rodriasdev <veronrodrigo98@gmail.com>
2024-05-23 19:10:43 -0300

    added the route 'test'

--- server/app.js
+++ server/app.js
@@ -6,6 +6,11 @@ const server = createServer();
 
 const PORT = process.env.PORT || 4000;
 
+app.get('/test', (_,res) => {
+    res.send('test')
+})
+
 app.listen(PORT, () => {
     console.log('server on in port' + PORT);
 });
+
