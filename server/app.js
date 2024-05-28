636dab15b17d1aa78ce336781d10a3444b367067
Rodriasd <veronrodrigo98@gmail.com>
2024-05-28 15:28:30 -0300

    changes

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import {createServer} from 'http';
-import { Server } from 'socket.io';
+// import express from 'express';
+// import {createServer} from 'http';
+// import { Server } from 'socket.io';
 import {LlamaModel, LlamaContext, LlamaChatSession} from "node-llama-cpp";
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -14,13 +14,29 @@ const model = new LlamaModel({
 const context = new LlamaContext({model});
 const session = new LlamaChatSession({context});
 
-const app = express();
-const server = createServer();
-const io = new Server(server);
 
-const PORT = process.env.PORT || 4000;
+const q1 = "Hi there, how are you?";
+console.log("User: " + q1);
 
-app.listen(PORT, () => {
-    console.log('server on in port' + PORT);
-});
+const a1 = await session.prompt(q1);
+console.log("AI: " + a1);
+
+
+const q2 = "Summerize what you said";
+console.log("User: " + q2);
+
+const a2 = await session.prompt(q2);
+console.log("AI: " + a2);
+
+
+
+// const app = express();
+// const server = createServer();
+// const io = new Server(server);
+
+// const PORT = process.env.PORT || 4000;
+
+// app.listen(PORT, () => {
+//     console.log('server on in port' + PORT);
+// });
 
